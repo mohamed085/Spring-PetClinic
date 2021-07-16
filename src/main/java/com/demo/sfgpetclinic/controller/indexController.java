981905b08eaba55b88b49d328d9879d604a50f73
mohamed085 <mohamed085555@gmail.com>
2021-07-16 18:42:22 +0200

    adding index page and index controller. Closes #4

---
+++ src/main/java/com/demo/sfgpetclinic/controller/indexController.java
@@ -0,0 +1,14 @@
+package com.demo.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
