1e113a05f0e20d6cde640660fa6043355a6e5343
mohamed085 <mohamed085555@gmail.com>
2021-07-17 14:58:34 +0200

    display all owners. Closes #8

--- src/main/java/com/demo/sfgpetclinic/bootstrap/DataLoader.java
+++ src/main/java/com/demo/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,5 +1,6 @@
 package com.demo.sfgpetclinic.bootstrap;
 
+import com.demo.sfgpetclinic.model.Owner;
 import com.demo.sfgpetclinic.services.OwnerServices;
 import com.demo.sfgpetclinic.services.VetServices;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +19,17 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Mohamed");
+        owner1.setLastName("Emad");
 
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Ali");
+        owner2.setLastName("Ahmed");
+
+        ownerServices.save(owner1);
+        ownerServices.save(owner2);
     }
 }
--- src/main/java/com/demo/sfgpetclinic/controller/OwnerController.java
+++ src/main/java/com/demo/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package com.demo.sfgpetclinic.controller;
 
+import com.demo.sfgpetclinic.services.OwnerServices;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listOwners() {
+    private final OwnerServices ownerServices;
 
+    public OwnerController(OwnerServices ownerServices) {
+        this.ownerServices = ownerServices;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerServices.findAll());
         return "owners/index";
     }
 }
