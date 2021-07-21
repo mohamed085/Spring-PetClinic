9db71a1cba32561c6bbd89d0cab151a2f36152c7
mohamed085 <mohamed085555@gmail.com>
2021-07-21 22:35:11 +0200

    add static resources. Closes #11

--- src/main/java/com/demo/sfgpetclinic/model/BaseEntity.java
+++ src/main/java/com/demo/sfgpetclinic/model/BaseEntity.java
@@ -1,16 +1,9 @@
 package com.demo.sfgpetclinic.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- src/main/java/com/demo/sfgpetclinic/model/Person.java
+++ src/main/java/com/demo/sfgpetclinic/model/Person.java
@@ -1,16 +1,10 @@
 package com.demo.sfgpetclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
 import java.util.Objects;
 
-@MappedSuperclass
 public class Person extends BaseEntity{
 
-    @Column(name = "first_name")
     private String firstName;
-
-    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
