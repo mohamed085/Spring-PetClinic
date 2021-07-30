d3741c35b0a528f0935816906893a3ae5342c1bd
mohamed085 <mohamed085555@gmail.com>
2021-07-30 21:06:48 +0200

    add pets and contact info to owners on startup. Closes #23

--- src/main/java/com/demo/sfgpetclinic/bootstrap/DataLoader.java
+++ src/main/java/com/demo/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.demo.sfgpetclinic.bootstrap;
 
 import com.demo.sfgpetclinic.model.Owner;
+import com.demo.sfgpetclinic.model.Pet;
 import com.demo.sfgpetclinic.model.PetType;
 import com.demo.sfgpetclinic.model.Vet;
 import com.demo.sfgpetclinic.services.OwnerServices;
@@ -8,6 +9,8 @@ import com.demo.sfgpetclinic.services.PetTypeServices;
 import com.demo.sfgpetclinic.services.VetServices;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,23 +39,44 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Mohamed");
         owner1.setLastName("Emad");
+        owner1.setAddress("123 bii");
+        owner1.setCity("asdas");
+        owner1.setTelephone("13252321");
+
+        Pet pet1 = new Pet();
+        pet1.setPetType(savedDogPetType);
+        pet1.setBirthDate(LocalDate.now());
+        pet1.setName("Rosco");
+
+        owner1.getPets().add(pet1);
+        ownerServices.save(owner1);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Ali");
         owner2.setLastName("Ahmed");
+        owner2.setAddress("123 bisi");
+        owner2.setCity("asdaass");
+        owner2.setTelephone("1325232221");
 
-        ownerServices.save(owner1);
+        Pet pet2 = new Pet();
+        pet2.setPetType(savedDogPetType);
+        pet2.setBirthDate(LocalDate.now());
+        pet2.setName("Tito");
+
+        owner2.getPets().add(pet2);
         ownerServices.save(owner2);
+
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Mohamed");
         vet1.setLastName("Emad");
+        vetServices.save(vet1);
+
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Ali");
         vet2.setLastName("Ahmed");
-
-        vetServices.save(vet1);
         vetServices.save(vet2);
 
     }
--- src/main/java/com/demo/sfgpetclinic/services/map/OwnerServicesMap.java
+++ src/main/java/com/demo/sfgpetclinic/services/map/OwnerServicesMap.java
@@ -1,13 +1,25 @@
 package com.demo.sfgpetclinic.services.map;
 
 import com.demo.sfgpetclinic.model.Owner;
+import com.demo.sfgpetclinic.model.Pet;
 import com.demo.sfgpetclinic.services.OwnerServices;
+import com.demo.sfgpetclinic.services.PetServices;
+import com.demo.sfgpetclinic.services.PetTypeServices;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServicesMap extends AbstractMapServices<Owner, Long> implements OwnerServices {
+
+    private final PetTypeServices petTypeServices;
+    private final PetServices petServices;
+
+    public OwnerServicesMap(PetTypeServices petTypeServices, PetServices petServices) {
+        this.petTypeServices = petTypeServices;
+        this.petServices = petServices;
+    }
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -30,7 +42,26 @@ public class OwnerServicesMap extends AbstractMapServices<Owner, Long> implement
 
     @Override
     public Owner save(Owner t) {
-        return super.save(t);
+
+        if (t != null) {
+            t.getPets().forEach(pet -> {
+                 if (pet.getPetType() != null) {
+                     if (pet.getPetType().getId() != null) {
+                         pet.setPetType(petTypeServices.save(pet.getPetType()));
+                     }
+                 } else {
+                     throw new RuntimeException("Pet Type is required");
+                 }
+
+                 if (pet.getId() == null) {
+                     Pet savedPet = petServices.save(pet);
+                     pet.setId(savedPet.getId());
+                 }
+            });
+            return super.save(t);
+        } else {
+            return null;
+        }
     }
 
     @Override
