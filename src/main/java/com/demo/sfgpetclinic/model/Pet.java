221d852181d4729416bd31a98fec247920f1abef
mohamed085 <mohamed085555@gmail.com>
2021-07-31 01:49:56 +0200

    create Visit JPA entity. Closes #28

--- src/main/java/com/demo/sfgpetclinic/model/Pet.java
+++ src/main/java/com/demo/sfgpetclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package com.demo.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +20,9 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,5 +54,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
