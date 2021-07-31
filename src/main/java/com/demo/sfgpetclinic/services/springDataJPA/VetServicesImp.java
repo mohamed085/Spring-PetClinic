e75074f721dc00e1df939e272dc9755d2361ef96
mohamed085 <mohamed085555@gmail.com>
2021-07-31 02:50:50 +0200

    implement VetServicesImp using spring sata JPA. Closes #31

---
+++ src/main/java/com/demo/sfgpetclinic/services/springDataJPA/VetServicesImp.java
@@ -0,0 +1,48 @@
+package com.demo.sfgpetclinic.services.springDataJPA;
+
+import com.demo.sfgpetclinic.model.Vet;
+import com.demo.sfgpetclinic.repositories.VetRepository;
+import com.demo.sfgpetclinic.services.VetServices;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springDataJPA")
+public class VetServicesImp implements VetServices {
+
+    private final VetRepository vetRepository;
+
+    public VetServicesImp(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
