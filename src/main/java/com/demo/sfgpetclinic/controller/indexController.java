69080a84e084e3b76174bc260cfe013f037ca5db
mohamed085 <mohamed085555@gmail.com>
2022-01-19 15:52:42 +0200

    Up to date

--- src/main/java/com/demo/sfgpetclinic/controller/indexController.java
+++ src/main/java/com/demo/sfgpetclinic/controller/indexController.java
@@ -1,19 +1,22 @@
 package com.demo.sfgpetclinic.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class indexController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String index() {
-
+        log.info("Request index Page");
         return "index";
     }
 
     @RequestMapping({ "/oups", "/oups.html"})
     public String oups() {
+        log.info("Request oups Page");
 
         return "NotImplemented";
     }
